8101e2351b841aee6ecd8ec1d81fe2f353917bfb
shaishav-rgb <shaishavpaudel@gmail.com>
2025-08-11 06:00:11 +0545

    feat: add type based on another user-defined type

--- main.go
+++ main.go
@@ -4,52 +4,42 @@ import (
 	"fmt"
 )
 
+type KathmanduTravels struct {
+	altitude int
+	location string
+}
+
+func (k KathmanduTravels) showPollution(pollutionLevel int) int {
+	return k.altitude + pollutionLevel
+}
+
+func (k KathmanduTravels) showHills() string {
+	return "Hilly Location:" + k.location
+}
+type LouisvilleTravels KathmanduTravels
+
+func (k LouisvilleTravels) showMallsCount(mallsCount int) int {
+	return k.altitude + mallsCount
+}
+
+func (k LouisvilleTravels) showRoads() string {
+	return "Road Location:" + k.location
+}
+
 func main() {
-	var f *int                           // f is nil
-	fmt.Printf("%p is nil pointer\n", f) // f is nil
-	var pointerOfPointer = &f
-	fmt.Printf("%p address of pointer of pointer\n", pointerOfPointer)
-	fmt.Printf("%p is value of pointer of pointer\n", *pointerOfPointer)
-	correctUpdateForNilPointer(&f)
-	// failedUpdateForNilPointer(f)
-	// fmt.Println(*f) // cannot deference a nil pointer, cannot compile compiler panics
-	// fmt.Println(f) // prints nil
+	kathmanduTravels:=KathmanduTravels{
+		altitude: 1200,
+		location: "Chandragiri",
+	}
 
-	// var f *int
-	// number := 10
-	// f = &number
-	// failedUpdateForNonNilPointer(f)
-	// correctUpdateForNonNilPointer(f)
-	fmt.Println("Outside function")
-	fmt.Println(*f)
-}
+	fmt.Println(kathmanduTravels.showPollution(1))
+	fmt.Println(kathmanduTravels.showHills())
 
-// func failedUpdateForNilPointer(g *int) {
-// 	x := 10
-// 	g = &x
-// }
+	louisvilleTravels:=LouisvilleTravels{
+		altitude: 400,
+		location: "Chandragiri",
+	}
 
-// func failedUpdateForNonNilPointer(g *int) {
-// 	fmt.Println(*g)
-// 	x := 100
-// 	g = &x
-// 	fmt.Println(*g)
-// }
-
-func correctUpdateForNilPointer(g **int) {
-	fmt.Println("Inside function")
-	x := 10
-	fmt.Printf("%p is pointer of assigned value\n", &x)
-	*g = &x
-	fmt.Printf("%p is pointer of pointer passed to function\n", g)
-	fmt.Printf("%p is single deference of pointer\n", *g)
-	fmt.Printf("%d is value of pointer of pointer inside function\n", **g)
-}
-
-func correctUpdateForNonNilPointer(g *int) {
-	fmt.Println(*g)
-	x := 100
-	*g = x
-	// g = x //cannot use x (variable of type int) as *int value in assignment
-	fmt.Println(*g)
+	fmt.Println(louisvilleTravels.showMallsCount(1))
+	fmt.Println(louisvilleTravels.showRoads())
 }
