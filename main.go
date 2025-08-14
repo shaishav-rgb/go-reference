283fea8b4670f7cc8642580df915aebd8e2659f8
shaishav-rgb <shaishavpaudel@gmail.com>
2025-08-14 08:24:42 +0545

    chore: add comments about generics to enforce same concrete type

--- main.go
+++ main.go
@@ -36,7 +36,9 @@ func main() {
 	houseValue := house{}
 	officeValue := office{}
 
-	fmt.Println(houseValue == house(officeValue))    //type has to match, gives true as struct is comparable type
-	fmt.Println(compareInterface(houseValue, officeValue))   // gives false because types are different
-	fmt.Println(compareInterface(houseValue, house(officeValue)))   // gives true because interface type and value type both match and struct is a comparable type
+	fmt.Println(houseValue == house(officeValue))                 //type has to match, gives true as struct is comparable type
+	fmt.Println(compareInterface(houseValue, officeValue))        // gives false because types are different
+	fmt.Println(compareInterface(houseValue, house(officeValue))) // gives true because interface type and value type both match and struct is a comparable type
+
+	fmt.Println(compareInterface(houseValue, officeValue)) // concrete types are different but allowed in compile, Generics can enforce the same concrete types for these two values that implements interface "locationer" at compile time
 }
