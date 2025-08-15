e185dd98c01eb881756fac35952c505d02f4687d
shaishav-rgb <shaishavpaudel@gmail.com>
2025-08-15 08:19:19 +0545

    feat: add type element reference

--- main.go
+++ main.go
@@ -2,65 +2,33 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strconv"
 )
 
-type Pair[T fmt.Stringer] struct {
-	Val1 T
-	Val2 T
+type Integer interface {
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64
 }
 
-type Differ[T any] interface {
-	fmt.Stringer
-	Diff(T) float64
+func Convert[T1, T2 Integer](in T1) T2 {
+	return T2(in)
 }
 
-func FindCloser[T Differ[T]](pair1, pair2 Pair[T]) Pair[T] {
-	d1 := pair1.Val1.Diff(pair1.Val2)
-	d2 := pair2.Val1.Diff(pair2.Val2)
-	if d1 < d2 {
-		return pair1
-	}
-	return pair2
+func Convert1[T1 ~int, T2 ~string](in T1, s T2) T2 {
+// func Convert1[T1 ~int, T2 string](in T1, s T2) T2 {
+	newString:=strconv.Itoa(int(in))+"s"
+	return T2(newString)
 }
 
-
-type Point2D struct {
-	X, Y int
-}
-
-func (p2 Point2D) String() string {
-	return fmt.Sprintf("{%d,%d}", p2.X, p2.Y)
-}
-func (p2 Point2D) Diff(from Point2D) float64 {
-	x := p2.X - from.X
-	y := p2.Y - from.Y
-	return math.Sqrt(float64(x*x) + float64(y*y))
-}
-
-type Point3D struct {
-	X, Y, Z int
-}
-
-func (p3 Point3D) String() string {
-	return fmt.Sprintf("{%d,%d,%d}", p3.X, p3.Y, p3.Z)
-}
-func (p3 Point3D) Diff(from Point3D) float64 {
-	x := p3.X - from.X
-	y := p3.Y - from.Y
-	z := p3.Z - from.Z
-	return math.Sqrt(float64(x*x) + float64(y*y) + float64(z*z))
-}
-
-func main() {
-	pair2Da := Pair[Point2D]{Point2D{1, 1}, Point2D{5, 5}}
-	pair2Db := Pair[Point2D]{Point2D{10, 10}, Point2D{15, 5}}
-	closer := FindCloser(pair2Da, pair2Db)
-	fmt.Println(closer)
-
-	pair3Da := Pair[Point3D]{Point3D{1, 1, 10}, Point3D{5, 5, 0}}
-	pair3Db := Pair[Point3D]{Point3D{10, 10, 10}, Point3D{11, 5, 0}}
-	closer2 := FindCloser(pair3Da, pair3Db)
-	fmt.Println(closer2)
-
+// func Convert1[T1 ~int, T2 string](in T1, s T2) T2 {
+// 	newString:=strconv.Itoa(int(in))+"s"
+// 	return T2(newString)
+// }
+func main() { 
+	var a int = 10
+	type newString string
+	 var stringValue newString= "hi"
+	b1 := Convert1(a,stringValue) // inferred as newString type. Parameter is also a user-defined type built from built-in type, can accept this  user-defined type because of "~" in front of type in parameter.
+	fmt.Println(b1)
+	b := Convert[int, int64](a) // can't infer the return type, in64 is not used inside the function, it is only a generic type.
+	fmt.Println(b)
 }
