78a4c69f8487fd990633ccbe60d386b875e8025a
shaishav-rgb <shaishavpaudel@gmail.com>
2025-08-16 15:58:44 +0545

    feat: add type element and generic type constraint reference

--- main.go
+++ main.go
@@ -2,33 +2,33 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-type Integer interface {
-	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64
+type IntChannel interface {
+	chan<- int | <-chan int
 }
 
-func Convert[T1, T2 Integer](in T1) T2 {
-	return T2(in)
+func useChannel[C IntChannel](ch C) {
+	fmt.Println("Got a channel:", ch)
 }
+func main() {
 
-func Convert1[T1 ~int, T2 ~string](in T1, s T2) T2 {
-// func Convert1[T1 ~int, T2 string](in T1, s T2) T2 {
-	newString:=strconv.Itoa(int(in))+"s"
-	return T2(newString)
-}
+	sendCh := make(chan int)
+	recvCh := make(chan int)
 
-// func Convert1[T1 ~int, T2 string](in T1, s T2) T2 {
-// 	newString:=strconv.Itoa(int(in))+"s"
-// 	return T2(newString)
-// }
-func main() { 
-	var a int = 10
-	type newString string
-	 var stringValue newString= "hi"
-	b1 := Convert1(a,stringValue) // inferred as newString type. Parameter is also a user-defined type built from built-in type, can accept this  user-defined type because of "~" in front of type in parameter.
-	fmt.Println(b1)
-	b := Convert[int, int64](a) // can't infer the return type, in64 is not used inside the function, it is only a generic type.
-	fmt.Println(b)
+	var _ = (chan<- int)(sendCh) // can use both variable as _
+	var _ = (<-chan int)(recvCh) // can name both variable as _
+	// fmt.Println(_) // cannot use _ as value or type
+
+	var sendChannel = (chan<- int)(sendCh)
+	var receiveChannel = (<-chan int)(recvCh)
+
+	useChannel(sendChannel)
+	// useChannel(sendCh)  // cannot use sendCh here, have to do type conversion
+	useChannel(receiveChannel)
+
+	//cannot use type terms as variable type, can be only used as generic type constraint
+	// var _ IntChannel = (chan<- int)(sendCh)
+	// var _ IntChannel = (<-chan int)(recvCh)
+
 }
