1358f7c5c09f8b75d48ef475dab15187f6899295
shaishav-rgb <shaishavpaudel@gmail.com>
2025-08-23 17:25:02 +0545

    feat: add unwrap error reference

---
+++ errors/multierror custom unwrap/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Status int
+
+type StatusErr struct {
+	Status  Status
+	Message string
+}
+
+func (se StatusErr) Error() string {
+	return se.Message
+}
+
+const (
+	InvalidLogin Status = iota + 1
+	NotFound
+)
+
+type MyError struct {
+	Code   int
+	Errors []error
+}
+
+func (m MyError) Error() string {
+	return errors.Join(m.Errors...).Error()
+}
+
+func (m MyError) Unwrap() []error {
+	return m.Errors
+}
+
+func funcThatReturnsAnError() error {
+	return MyError{
+		Code: 12,
+		Errors: []error{
+			StatusErr{
+				Status:  NotFound,
+				Message: "file Not Found",
+			},
+			errors.New("a simple string error"),
+		},
+	}
+}
+
+func main() {
+	var err error
+	err = funcThatReturnsAnError()
+	switch err := err.(type) {
+	case interface{ Unwrap() error }:
+		// handle single error
+		innerErr := err.Unwrap()
+		// process innerErr
+		fmt.Println(innerErr)
+	case interface{ Unwrap() []error }:
+		//handle multiple wrapped errors
+		innerErrs := err.Unwrap()
+		fmt.Println(innerErrs) // print does not call individual Error() method on the error slice, this just prints the []error slice
+		for _, innerErr := range innerErrs {
+			// process each innerErr
+			fmt.Println(innerErr)  // this calls Error() method in individual error.
+		}
+	default:
+		// handle no wrapped error
+	}
+}
