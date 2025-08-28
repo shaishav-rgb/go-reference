3e03463957d466da086cb706536dfabdd32ff714
shaishav-rgb <shaishavpaudel@gmail.com>
2025-08-28 07:28:37 +0545

    feat: add cobra module

--- go-package/main.go
+++ go-package/main.go
@@ -3,18 +3,27 @@ package main //package should be named main to run
 
 import (
 	"fmt"
-	initpackage "test-go-module-package/init-package" // referencing initpackage so init gets called before anything in main function
+	// initpackage "test-go-module-package/init-package" // referencing initpackage so init gets called before anything in main function
 
 	// "test-go-module-package/parent/sibling"
-	"test-go-module-package/product"
+	// "test-go-module-package/product"
+	"github.com/spf13/cobra"
 )
 
 // func main1(){      //Error, function should be named main to run
 func main() { //function should be named main to run
-	fmt.Println("Calling main function")
-	initpackage.SayHi()
-	product.ProductMessage("This is product message")
+	// fmt.Println("Calling main function")
+	// initpackage.SayHi()
+	// product.ProductMessage("This is product message")
 	// parent.CallInternal()
 	// sibling.CallInternal()
 	// product.CircularProductCall()
+	cmd := &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello, Modules!")
+
+		},
+	}
+	fmt.Println("Calling cmd.Execute()!")
+	cmd.Execute()
 }
