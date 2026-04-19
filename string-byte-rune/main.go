b778807c0b8c7af6f68ba18a861858ebb92ac533
shaishav-rgb <shaishavpaudel@gmail.com>
2026-04-19 06:57:37 +0545

    feat: add rune and for range

--- string-byte-rune/main.go
+++ string-byte-rune/main.go
@@ -11,8 +11,35 @@ func main() {
 	fmt.Println(string(s[0]))
 	//print characters
 	fmt.Println(s[0:5])
-	var b byte = s[6]
+	//the type of b is byte
+	 b:= s[6]
 	fmt.Println(b)
+
+	fmt.Println("------------------------------")
+
+	var s1 string = "Hello😎"
+
+	b1:=[]byte(s1)
+	r1:=[]rune(s1)
+
+	for _,value:=range b1{
+		fmt.Println(value)
+		fmt.Println(string(value))
+	}
+	fmt.Println("------------------------------")
+	
+	for _,value:=range r1{
+		fmt.Println(value)
+		fmt.Println(string(value))
+	}
+
+	fmt.Println("------------------------------")
+
+	//type of value is rune
+	for _,value:=range s1{
+		fmt.Println(value)
+		fmt.Println(string(value))
+	}
 }
 
 
